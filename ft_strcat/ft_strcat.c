4812a3d688e3e97036030c22cd0dc87dd34cdbd5
admin <admin@Kseniias-MacBook-Air.local>
2017-12-02 00:11:16 +0200

    ft_strcat is added

---
+++ ft_strcat/ft_strcat.c
@@ -0,0 +1,20 @@
+#include "libft.h"
+
+char *ft_strcat(char *s1, const char *s2)
+{
+	char	*s_final;
+
+	s_final = s1;
+
+	while (*s1 != '\0')
+	{
+		s1++;
+	}
+	while (*s2 != '\0')
+	{
+		*s1 = *s2;
+		s1++;
+		s2++;
+	}
+	return (s_final);
+}
---
+++ ft_strcat/libft.h
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kprytkov <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/11/22 16:48:29 by kprytkov          #+#    #+#             */
+/*   Updated: 2017/11/22 16:48:30 by kprytkov         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_LIBFT_H
+# define FT_LIBFT_H
+# include <string.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
+
+void	ft_putchar(char c);
+void	ft_putstr(char const *s);
+void	*ft_memset(void *b, int c, size_t len);
+void	ft_bzero(void *s, size_t n);
+size_t	ft_strlen(const char *s);
+void 	*ft_memcpy(void *dst, const void *src, size_t n);
+void 	*ft_memccpy(void *dst, const void *src, int c, size_t n);
+void 	*ft_memmove(void *dst, const void *src, size_t len);
+char 	*ft_strdup(const char *s1);
+char	*ft_strcpy(char *dst, const char *src);
+char 	*ft_strncpy(char *dst, const char *src, size_t len);
+void 	*ft_memchr(const void *s, int c, size_t n);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
+char 	*ft_strcat(char *s1, const char *s2);
+
+#endif
