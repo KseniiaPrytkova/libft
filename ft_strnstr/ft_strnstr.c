5881371c30cb38214bc5ab493e1e4282a74031f5
kseniiaprytkova <prytkovakseniia@gmail.com>
2017-12-06 01:21:53 +0200

    ft_strstr && ft_strnstr are added

---
+++ ft_strnstr/ft_strnstr.c
@@ -0,0 +1,43 @@
+#include "libft.h"
+
+int		ft_substrcmp(const char *s1, const char *s2, size_t n)
+{
+	size_t counter;
+
+	counter = 0;
+	while (counter < n && (*s1 - *s2) == 0)
+	{
+		s1++;
+		s2++;
+		counter++;
+	}
+	return (counter == n);
+}
+
+char 	*ft_strnstr(const char *haystack, const char *needle, size_t len)
+{
+	size_t smallstr_len;
+	size_t counter;
+	int finish;
+
+	smallstr_len = ft_strlen(needle);
+	
+	finish = len - smallstr_len;
+	if (finish <= 0)
+		return (NULL);
+
+	counter = 0;
+	if (*needle == '\0')
+		return ((char *)haystack);
+	
+	while (counter <= finish)
+	{
+
+		if (ft_substrcmp(haystack, needle, smallstr_len) == 1)
+			return ((char *)haystack);
+
+		haystack++;
+		counter++;
+	}
+	return (NULL);
+}
---
+++ ft_strnstr/main.c
@@ -0,0 +1,57 @@
+#include "libft.h"
+
+int		main(void)
+{
+	/*___TEST_CASE_1___*/
+	char	*largerstring_std = "Foo Bar Baz okay haha";
+	char	*smallstring_std = "okay";
+	int		len_std = 17;
+	char	*ptr_std;
+
+	ptr_std = strnstr(largerstring_std, smallstring_std, len_std);
+	printf("TEST_CASE_1:\n");
+	printf("I'm standart: %s\n",ptr_std);
+	/*----------------------------------------------------------------------*/
+
+	char	*ptr_my;
+
+	ptr_my = ft_strnstr(largerstring_std, smallstring_std, len_std);
+	printf("I'm your's:   %s\n",ptr_my);
+
+	/*___TEST_CASE_2___*/
+	char	*largerstring_std2 = "someoneonthetoad";
+	char	*smallstring_std2;
+	int		len_std2 = 10;
+	char	*ptr_std2;
+
+	ptr_std2 = strnstr(largerstring_std2, smallstring_std2, len_std2);
+	printf("TEST_CASE_2:\n");
+	printf("I'm standart: %s\n",ptr_std2);
+	/*----------------------------------------------------------------------*/
+	char	*largerstring_my2 = "someoneonthetoad";
+	char	*smallstring_my2;
+	int		len_my2 = 10;
+	char	*ptr_my2;
+
+	ptr_my2 = ft_strnstr(largerstring_my2, smallstring_my2, len_my2);
+	printf("I'm your's:   %s\n",ptr_my2);
+
+	/*___TEST_CASE_3___*/
+	char	*largerstring_std3 = "someoneonthetoad";
+	char	*smallstring_std3 = "wolf";
+	int		len_std3 = 10;
+	char	*ptr_std3;
+
+	ptr_std3 = strnstr(largerstring_std3, smallstring_std3, len_std3);
+	printf("TEST_CASE_3:\n");
+	printf("I'm standart: %s\n",ptr_std3);
+	/*----------------------------------------------------------------------*/
+	char	*largerstring_my3 = "someoneonthetoad";
+	char	*smallstring_my3 = "wolf";
+	int		len_my3 = 10;
+	char	*ptr_my3;
+
+	ptr_my3 = ft_strnstr(largerstring_my3, smallstring_my3, len_my3);
+	printf("I'm your's:   %s\n",ptr_my3);
+	return (0);
+}
