c0e00eed575cd1ed4f216c68bc6c23f6ffafeaae
Kseniia PRYTKOVA <kprytkov@e1r1p9.unit.ua>
2017-12-07 22:19:43 +0200

    ft_isprint is on the way

---
+++ ft_isprint/main.c
@@ -0,0 +1,19 @@
+#include "libft.h"
+
+int		main(void)
+{
+	/*___TEST_CASE_1___*/
+	char c = '5';
+	printf("TEST_CASE_1:\n");
+	printf("I'm standart: %i\n", isprint(c));
+
+	// printf("I'm your's:   %i\n", ft_isascii(c));
+
+	/*___TEST_CASE_2___*/
+	char c_2 = " ";
+	printf("TEST_CASE_2:\n");
+	printf("I'm standart: %i\n", isprint(c_2));
+
+	// printf("I'm your's:   %i\n", ft_isascii(c_2));
+	return (0);
+}
