a23309bd22b9fe5fd7ba200a77f8aee9c708762c
Kseniia PRYTKOVA <kprytkov@e1r1p4.unit.ua>
2017-12-08 17:07:05 +0200

    ft_tolower was added

--- ft_tolower/ft_tolower.c
+++ ft_tolower/ft_tolower.c
@@ -1,22 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_toupper.c                                       :+:      :+:    :+:   */
+/*   ft_tolower.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: kprytkov <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/12/08 16:44:19 by kprytkov          #+#    #+#             */
-/*   Updated: 2017/12/08 16:44:20 by kprytkov         ###   ########.fr       */
+/*   Created: 2017/12/08 17:03:25 by kprytkov          #+#    #+#             */
+/*   Updated: 2017/12/08 17:03:26 by kprytkov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_toupper(int c)
+int		ft_tolower(int c)
 {
 	unsigned char c_transform;
 
-	if ((unsigned char)c >= 97 && (unsigned char)c <= 122)
+	if ((unsigned char)c >= 65 && (unsigned char)c <= 90)
 	{
-		c_transform = (unsigned char)c - 32;
+		c_transform = (unsigned char)c + 32;
 		return (c_transform);
 	}
 	return (c);
