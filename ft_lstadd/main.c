e3788ea8f5da7f487e40c1fe9da60747bd194b47
kseniiaprytkova <prytkovakseniia@gmail.com>
2017-12-19 22:47:38 +0200

    Fixes

--- ft_lstadd/main.c
+++ ft_lstadd/main.c
@@ -4,12 +4,12 @@ int		main(void)
 {
 	/*___TEST_CASE_1___*/
 	t_list *add_this;
-	t_list *element;
+	t_list *element = NULL;
 
 	add_this = ft_lstnew("kaka", 5);
-	element = ft_lstnew("dadaspusss", 11);
+	// element = ft_lstnew("dadaspusss", 11);
 	printf("TEST_CASE_1:\n");
-	printf("before:  %s\n", element->content);
+	// printf("before:  %s\n", element->content);
 
 	ft_lstadd(&element, add_this);
 
