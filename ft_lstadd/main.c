48a42017c978c8294856e1c5d87d8aafca8b6ea3
kseniiaprytkova <prytkovakseniia@gmail.com>
2017-12-19 22:42:18 +0200

    ft_lstadd was added

---
+++ ft_lstadd/main.c
@@ -0,0 +1,19 @@
+#include "libft.h"
+
+int		main(void)
+{
+	/*___TEST_CASE_1___*/
+	t_list *add_this;
+	t_list *element;
+
+	add_this = ft_lstnew("kaka", 5);
+	element = ft_lstnew("dadaspusss", 11);
+	printf("TEST_CASE_1:\n");
+	printf("before:  %s\n", element->content);
+
+	ft_lstadd(&element, add_this);
+
+	printf("after:   %s\n", element->content);
+
+	return (0);
+}
