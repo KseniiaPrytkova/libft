84548b7d4bd056b4d53d5e3f4b1745898da6674e
Kseniia PRYTKOVA <kprytkov@e1r3p9.unit.ua>
2017-12-20 19:11:22 +0200

    function ft_strsplit has 26 lines

--- final/ft_strsplit.c
+++ final/ft_strsplit.c
@@ -1,36 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strsplit.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kprytkov <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/12/20 19:02:23 by kprytkov          #+#    #+#             */
+/*   Updated: 2017/12/20 19:02:24 by kprytkov         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
-size_t len_of_word(char *s, char c)
+size_t	len_of_word(char *s, char c)
 {
 	size_t counter;
 
 	counter = 0;
 	while (*s != '\0' && *s != c)
 	{
-
 		counter++;
 		s++;
 	}
-	
 	return (counter);
 }
 
-size_t copy(char *dst, char *src, size_t len)
+size_t	copy(char *dst, char *src, size_t len)
 {
 	ft_strncpy(dst, (char *)src, len);
 	return (len);
 }
 
-char	** ft_strsplit(char const *s, char c)
+char	**ft_strsplit(char const *s, char c)
 {
-	char **storage;
-	size_t stor_size;
-	size_t one_word_len;
+	char	**storage;
+	size_t	stor_size;
+	size_t	one_word_len;
 
 	storage = NULL;
-    if (s)
-    {	
-    	stor_size = ft_wordcount(s, c);
+	if (s)
+	{
+		stor_size = ft_wordcount(s, c);
 		if (!(storage = malloc(sizeof(char *) * stor_size + 1)))
 			return (NULL);
 		while (*s != '\0')
@@ -42,7 +52,7 @@ char	** ft_strsplit(char const *s, char c)
 					return (NULL);
 				s = s + copy(*storage, (char *)s, one_word_len) - 1;
 				storage++;
-			} 
+			}
 			s++;
 		}
 		*storage = NULL;
